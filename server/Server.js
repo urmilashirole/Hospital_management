23c38259600070c579defbd86098c3b4d8998773
urmilashirole <161549377+urmilashirole@users.noreply.github.com>
2024-04-05 22:19:22 +0530

    Add files via upload

---
+++ server/Server.js
@@ -0,0 +1,34 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const patientsRouter = require('./Routes/patients');
+const doctorsRouter = require('./Routes/doctors');
+const appoinmentsRouter = require('./Routes/Appointments')
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(
+	'mongodb://localhost:27017/hospital',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log('MongoDB database connection established successfully');
+});
+
+app.use('/patients', patientsRouter);
+app.use('/doctors', doctorsRouter);
+app.use('/appointments', appoinmentsRouter)
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
